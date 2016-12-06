24bc72575c3e16123ed73316a1bd7965d1cacaf4
ruedagato <ruedagato@gmail.com>
2016-12-06 11:31:28 -0500

    init comit

---
+++ src/app/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+	config : {
+    apiKey: "AIzaSyA_TopHOY_rkNrFXS1DH3awfR14vmBiXYM",
+    authDomain: "runtour-11f91.firebaseapp.com",
+    databaseURL: "https://runtour-11f91.firebaseio.com",
+    storageBucket: "runtour-11f91.appspot.com",
+    messagingSenderId: "165845604330"
+  }
+};
